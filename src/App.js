22b4305563fe7a977274cdd37952eeb6f8748c30
ashfaqmbd <ashfaqmbd@gmail.com>
2020-06-06 14:05:27 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <b>Hello World</b> to All
+    </div>
+  );
+}
+
+export default App;
